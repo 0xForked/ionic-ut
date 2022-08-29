c80a58d8b42c0ed735e755a703307098ce00d0e3
A. A. Sumitro <hello@aasumitro.id>
2022-08-29 20:39:53 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exp-ionic-ut',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
